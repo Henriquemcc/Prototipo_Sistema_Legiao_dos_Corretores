e4ef411941204a6cdc12981251e08693e05d0330
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-10-31 11:46:56 -0300

    Adicionando conexão com o banco de dados

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,16 +19,19 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-docker-compose:3.3.4")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.5")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.5")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers:3.3.4")
 	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.testcontainers:mysql:1.20.1")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
