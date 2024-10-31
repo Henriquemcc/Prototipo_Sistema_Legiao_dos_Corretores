ef95ce9412595c16b7e03faaf46f7727dc99b8b4
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-10-31 12:47:24 -0300

    Corrigindo rota

--- src/main/kotlin/br/com/legiaodoscorretores/sistema/controller/UsuarioController.kt
+++ src/main/kotlin/br/com/legiaodoscorretores/sistema/controller/UsuarioController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/usuarios")
 class UsuarioController(
     private val service: UsuarioService
 )
