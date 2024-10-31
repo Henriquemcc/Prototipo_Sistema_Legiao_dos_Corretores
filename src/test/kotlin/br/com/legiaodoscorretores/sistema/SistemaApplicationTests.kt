f035592c02be10ef5d03d24c8e49d5a26e0a3a66
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-10-31 11:04:30 -0300

    Adicionando arquivos gerados pelo 'Spring Initializr' em 'start.spring.io'.

---
+++ src/test/kotlin/br/com/legiaodoscorretores/sistema/SistemaApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.legiaodoscorretores.sistema
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
