96d59ec8d765b4f96618240debe76ad6617eaec3
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-10-31 12:47:43 -0300

    Implementando listagem de usuários

--- src/main/kotlin/br/com/legiaodoscorretores/sistema/controller/UsuarioController.kt
+++ src/main/kotlin/br/com/legiaodoscorretores/sistema/controller/UsuarioController.kt
@@ -1,11 +1,19 @@
 package br.com.legiaodoscorretores.sistema.controller
 
+import br.com.legiaodoscorretores.sistema.model.Usuario
 import br.com.legiaodoscorretores.sistema.service.UsuarioService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/usuarios")
 class UsuarioController(
     private val service: UsuarioService
-)
+) {
+    @GetMapping
+    fun listar(): List<Usuario> {
+        return service.listar()
+    }
+}
--- src/main/kotlin/br/com/legiaodoscorretores/sistema/service/UsuarioService.kt
+++ src/main/kotlin/br/com/legiaodoscorretores/sistema/service/UsuarioService.kt
@@ -1,9 +1,14 @@
 package br.com.legiaodoscorretores.sistema.service
 
+import br.com.legiaodoscorretores.sistema.model.Usuario
 import br.com.legiaodoscorretores.sistema.repository.UsuarioRepository
 import org.springframework.stereotype.Service
 
 @Service
 class UsuarioService(
-    private val repository: UsuarioRepository
-)
+    private val usuarioRepository: UsuarioRepository
+) {
+    fun listar(): List<Usuario> {
+        return usuarioRepository.findAll()
+    }
+}
