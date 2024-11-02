ca5d38eb5cdb48fa37b96030cab1af54ccd9dd32
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-11-02 19:53:13 -0300

    Adicionando mapeamento entre NovaPessoaForm e Pessoa

---
+++ src/main/kotlin/br/com/legiaodoscorretores/sistema/mapper/PessoaFormMapper.kt
@@ -0,0 +1,17 @@
+package br.com.legiaodoscorretores.sistema.mapper
+
+import br.com.legiaodoscorretores.sistema.dto.NovaPessoaForm
+import br.com.legiaodoscorretores.sistema.model.Pessoa
+
+class PessoaFormMapper: Mapper<NovaPessoaForm, Pessoa> {
+    override fun map(t: NovaPessoaForm): Pessoa {
+        return Pessoa(
+            nome = t.nome,
+            sobrenome = t.sobrenome,
+            rg = t.rg,
+            cpf = t.cpf,
+            telefone = t.telefone,
+            email = t.email
+        )
+    }
+}
