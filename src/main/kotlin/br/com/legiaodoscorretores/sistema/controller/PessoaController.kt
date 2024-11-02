1f690e941bd622d531ce270b20b2ebbf22c602ba
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-11-02 20:13:06 -0300

    Substituindo o uso do pessoaService pelo pessoaDtoService

--- src/main/kotlin/br/com/legiaodoscorretores/sistema/controller/PessoaController.kt
+++ src/main/kotlin/br/com/legiaodoscorretores/sistema/controller/PessoaController.kt
@@ -1,42 +1,36 @@
 package br.com.legiaodoscorretores.sistema.controller
 
-import br.com.legiaodoscorretores.sistema.model.Pessoa
-import br.com.legiaodoscorretores.sistema.service.PessoaService
+import br.com.legiaodoscorretores.sistema.dto.NovaPessoaForm
+import br.com.legiaodoscorretores.sistema.dto.PessoaView
+import br.com.legiaodoscorretores.sistema.service.PessoaDtoService
 import jakarta.transaction.Transactional
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/pessoas")
 class PessoaController(
-    private val pessoaService: PessoaService
+    private val pessoaDtoService: PessoaDtoService
 ) {
     @GetMapping
-    fun listar(): List<Pessoa> {
-        return pessoaService.listar()
+    fun listar(): List<PessoaView> {
+        return pessoaDtoService.listar()
     }
 
     @PostMapping
     @Transactional
-    fun cadastrar(@RequestBody pessoa: Pessoa) {
-        pessoaService.cadastrar(pessoa)
+    fun cadastrar(@RequestBody novaPessoaForm: NovaPessoaForm) {
+        pessoaDtoService.cadastrar(novaPessoaForm)
     }
 
     @PutMapping("/{idPessoa}")
     @Transactional
-    fun atualizar(@RequestBody pessoa: Pessoa, @PathVariable idPessoa: Long) {
-        pessoaService.atualizar(pessoa, idPessoa)
+    fun atualizar(@RequestBody novaPessoaForm: NovaPessoaForm, @PathVariable idPessoa: Long) {
+        pessoaDtoService.atualizar(novaPessoaForm, idPessoa)
     }
 
     @DeleteMapping("/{idPessoa}")
     @Transactional
     fun deletar(@PathVariable idPessoa: Long) {
-        pessoaService.deletar(idPessoa)
+        pessoaDtoService.deletar(idPessoa)
     }
 }
