342fdf398a90dc2cb30d9d7aea113033887f3d68
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-11-02 20:02:34 -0300

    Adicionando mapeamento entre Pessoa e PessoaView

---
+++ src/main/kotlin/br/com/legiaodoscorretores/sistema/mapper/PessoaViewMapper.kt
@@ -0,0 +1,18 @@
+package br.com.legiaodoscorretores.sistema.mapper
+
+import br.com.legiaodoscorretores.sistema.dto.PessoaView
+import br.com.legiaodoscorretores.sistema.model.Pessoa
+
+class PessoaViewMapper: Mapper<Pessoa, PessoaView> {
+    override fun map(t: Pessoa): PessoaView {
+        return PessoaView(
+            id = t.id,
+            nome = t.nome,
+            sobrenome = t.sobrenome,
+            rg = t.rg,
+            cpf = t.cpf,
+            telefone = t.telefone,
+            email = t.email
+        )
+    }
+}
