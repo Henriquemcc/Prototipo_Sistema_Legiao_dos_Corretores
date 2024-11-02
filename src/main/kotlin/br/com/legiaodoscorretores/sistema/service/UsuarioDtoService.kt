02c4b07e766c63041af219fd6a0db260c3cf7a2e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-11-02 20:38:14 -0300

    Adicionando service para o Dto de Usuário

---
+++ src/main/kotlin/br/com/legiaodoscorretores/sistema/service/UsuarioDtoService.kt
@@ -0,0 +1,30 @@
+package br.com.legiaodoscorretores.sistema.service
+
+import br.com.legiaodoscorretores.sistema.dto.NovoUsuarioForm
+import br.com.legiaodoscorretores.sistema.dto.UsuarioView
+import br.com.legiaodoscorretores.sistema.mapper.UsuarioFormMapper
+import br.com.legiaodoscorretores.sistema.mapper.UsuarioViewMapper
+import org.springframework.stereotype.Service
+
+@Service
+class UsuarioDtoService(
+    private val usuarioService: UsuarioService,
+    private val usuarioViewMapper: UsuarioViewMapper,
+    private val usuarioFormMapper: UsuarioFormMapper
+) {
+    fun listar(): List<UsuarioView> {
+        return usuarioService.listar().map { u -> usuarioViewMapper.map(u) }
+    }
+
+    fun cadastrar(novoUsuarioForm: NovoUsuarioForm) {
+        usuarioService.cadastrar(usuarioFormMapper.map(novoUsuarioForm))
+    }
+
+    fun atualizar(novoUsuarioForm: NovoUsuarioForm, idUsuario: Long) {
+        usuarioService.atualizar(usuarioFormMapper.map(novoUsuarioForm), idUsuario)
+    }
+
+    fun deletar(idUsuario: Long) {
+        usuarioService.deletar(idUsuario)
+    }
+}
