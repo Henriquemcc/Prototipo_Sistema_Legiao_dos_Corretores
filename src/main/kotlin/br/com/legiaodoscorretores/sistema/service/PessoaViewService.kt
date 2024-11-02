56254afba55ea1b4a46add986103ceed2c0a64f3
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-11-02 20:10:06 -0300

    Adicionando service de Pessoa para Form e View

---
+++ src/main/kotlin/br/com/legiaodoscorretores/sistema/service/PessoaViewService.kt
@@ -0,0 +1,32 @@
+package br.com.legiaodoscorretores.sistema.service
+
+import br.com.legiaodoscorretores.sistema.dto.NovaPessoaForm
+import br.com.legiaodoscorretores.sistema.dto.PessoaView
+import br.com.legiaodoscorretores.sistema.mapper.PessoaFormMapper
+import br.com.legiaodoscorretores.sistema.mapper.PessoaViewMapper
+import org.springframework.stereotype.Service
+
+@Service
+class PessoaViewService(
+    val pessoaService: PessoaService,
+    val pessoaViewMapper: PessoaViewMapper,
+    val pessoaFormMapper: PessoaFormMapper
+) {
+    fun listar(): List<PessoaView> {
+        return pessoaService.listar().map {
+            p -> pessoaViewMapper.map(p)
+        }
+    }
+
+    fun cadastrar(novaPessoaForm: NovaPessoaForm) {
+        pessoaService.cadastrar(pessoaFormMapper.map(novaPessoaForm))
+    }
+
+    fun atualizar(novaPessoaForm: NovaPessoaForm, idPessoa: Long) {
+        pessoaService.atualizar(pessoaFormMapper.map(novaPessoaForm), idPessoa)
+    }
+
+    fun deletar(idPessoa: Long) {
+        pessoaService.deletar(idPessoa)
+    }
+}
