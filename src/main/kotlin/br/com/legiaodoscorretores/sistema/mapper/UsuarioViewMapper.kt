e3564592d7dc1252d3e4b6e4d3fa72536cdf8294
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-11-02 20:50:44 -0300

    Adicionando anotação '@Component'

--- src/main/kotlin/br/com/legiaodoscorretores/sistema/mapper/UsuarioViewMapper.kt
+++ src/main/kotlin/br/com/legiaodoscorretores/sistema/mapper/UsuarioViewMapper.kt
@@ -2,7 +2,9 @@ package br.com.legiaodoscorretores.sistema.mapper
 
 import br.com.legiaodoscorretores.sistema.dto.UsuarioView
 import br.com.legiaodoscorretores.sistema.model.Usuario
+import org.springframework.stereotype.Component
 
+@Component
 class UsuarioViewMapper: Mapper<Usuario, UsuarioView> {
     override fun map(t: Usuario): UsuarioView {
         return UsuarioView(
